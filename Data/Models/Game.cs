74ef5fb40d6400c078a6807e77ee0f16d1ec0aba
YoyoMy <35624284+YoyoMy@users.noreply.github.com>
2019-03-17 09:16:07 +0000

    Add files via upload
    Added Business//Accounts in Data

---
+++ Data/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Date { get; set; }
+        public string Developer { get; set; }
+        public string Genre { get; set; }
+        public string Image { get; set; }
+    }
+}
